0bf81daca3803046de07b2b3b3f66654d3982a5d
misikora <mikolajsi@student.agh.edu.pl>
2020-04-27 10:36:23 +0200

    Bug fix

--- Sikora_Mikolaj_4/Zadanie_1/src/client/Client.py
+++ Sikora_Mikolaj_4/Zadanie_1/src/client/Client.py
@@ -4,7 +4,7 @@ import sys
 from grpc._channel import _MultiThreadedRendezvous
 from src.gen import event_pb2 as event, event_pb2_grpc as eventGrpc
 
-sys.path = "../../.."
+# sys.path = "../../.."
 
 channel = grpc.insecure_channel('localhost:50051')
 stub = eventGrpc.EventNotifyStub(channel)
@@ -20,7 +20,7 @@ for cityNumber in cityNumbers:
 print('Provide Type number')
 for e in event._EVENTTYPE.values:
     print(str(e.index) + ' (' + e.name + ')')
-    print('------------')
+print('------------')
 
 subscribedEvent = event._EVENTTYPE.values[int(input())].index
 request = event.Subscription(cities=list(cities), event=subscribedEvent)
@@ -30,7 +30,11 @@ def subscribe(request):
     stream = stub.GetInterestingEvents(request)
     try:
         for e in stream:
-            print(e)
+            print(event._EVENTTYPE.values[e.event].name)
+            print(e.description)
+            print(e.organizers)
+            print(event._CITY.values[e.city].name)
+            print('------------')
     except _MultiThreadedRendezvous:
         return
 
